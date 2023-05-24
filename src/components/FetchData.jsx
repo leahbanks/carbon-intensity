3fbc8dd40c9039079cb3fe25074aba9d078db00e
Leah <leahb_96@hotmail.co.uk>
2023-05-24 14:28:47 +0100

    refactor and renamed file

---
+++ src/components/FetchData.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { DisplayData } from "./DisplayData";
+
+
+export function FetchData ({location}) {
+    const [carbonData, setCarbonData] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(()=>{
+        fetch(`https://api.carbonintensity.org.uk/regional/postcode/${location}`)
+         .then((response)=> {return (response.json())})
+         .then((body)=> {
+            setCarbonData(body)
+            setIsLoading(false)
+        })
+    }, [location])
+    if (isLoading){
+        return (<p>Loading...</p>)
+    }
+    else
+    return (
+        <section>
+            <h2>Results</h2>
+            <DisplayData carbonData={carbonData}/>
+        </section>
+    )
+}
