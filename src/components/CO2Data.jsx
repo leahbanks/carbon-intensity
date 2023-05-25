86720593990bc5190e0bb3a2ce4d2d7da8996d09
Leah <leahb_96@hotmail.co.uk>
2023-05-25 13:14:52 +0100

    edited postcode form

--- src/components/CO2Data.jsx
+++ src/components/CO2Data.jsx
@@ -1,42 +1,38 @@
 import { useState } from "react";
-import { GetData } from "./GetData";
+import { FetchData } from "./FetchData";
 import "../App.css";
 
 export function CO2Data() {
   const [location, setLocation] = useState("");
   const [hasLocation, setHasLocation] = useState(false);
-  if (!hasLocation) {
-    return (
-      <section>
-        <form type="submit">
-          <label htmlFor="search">
-            Outward Postcode (first 2-3 characters of postcode (M1)):{" "}
-          </label>
 
-          <input
-            type="text"
-            id="search"
-            onChange={(event) => {
-              setLocation(event.target.value);
-            }}
-          ></input>
+  return !hasLocation ? (
+    <section>
+      <form type="submit">
+        <label htmlFor="search">
+          Enter the first half of your postcode (e.g SW1A):{" "}
+        </label>
 
-          <button
-            onClick={(event) => {
-              event.preventDefault();
-              setHasLocation(true);
-            }}
-          >
-            Submit
-          </button>
-        </form>
-      </section>
-    );
-  } else {
-    return (
-      <section className="App">
-        <GetData location={location} />
-      </section>
-    );
-  }
+        <input
+          type="text"
+          id="search"
+          onChange={(event) => {
+            setLocation(event.target.value);
+          }}
+        ></input>
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            setHasLocation(true);
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </section>
+  ) : (
+    <section className="App">
+      <FetchData location={location} />
+    </section>
+  );
 }
